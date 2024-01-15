fd18df02b029a5dcf2c158bd3c9ab1caa74a0514
Purva Athnere <93878803+purvathnere@users.noreply.github.com>
2024-01-16 01:26:00 +0530

    Create Valid Parenthesis.cpp

---
+++ Valid Parenthesis.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+bool isvalid(string s)
+{
+    int n = s.size(); // Length of a string 
+    stack<char> st;    // Stack creation
+    bool ans = true;   // Suppose the string is already balanced
+
+    // Traverse the string 
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+        {
+            st.push(s[i]);
+        }
+        else if (s[i] == ')')
+        {
+            if (!st.empty() && st.top() == '(')
+            {
+                st.pop();
+            }
+            else
+            {
+                ans = false;
+                break;
+            }
+        }
+        else if (!st.empty() && s[i] == '}')
+        {
+            if (st.top() == '{')
+            {
+                st.pop();
+            }
+            else
+            {
+                ans = false;
+                break;
+            }
+        }
+        else if (!st.empty() && s[i] == ']')
+        {
+            if (st.top() == '[')
+            {
+                st.pop();
+            }
+            else
+            {
+                ans = false;
+                break;
+            }
+        }
+    }
+
+    return ans && st.empty();
+}
+
+int main()
+{
+    string str = "[{()}]";
+
+    if (isvalid(str))
+    {
+        cout << "String is valid" << endl;
+    }
+    else
+    {
+        cout << "String is invalid" << endl;
+    }
+
+    return 0;
+}
