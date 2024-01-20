803ab1e5092e97e71ae93e3ce340b0254396c5de
Purva Athnere <93878803+purvathnere@users.noreply.github.com>
2024-01-20 17:42:00 +0530

    Create k largest elements.cpp

---
+++ k largest elements.cpp
@@ -0,0 +1,14 @@
+class Solution{
+public:	
+	vector<int> kLargest(int arr[], int n, int k) {
+	 sort(arr,arr+n);
+	 reverse(arr,arr+n);
+	 vector<int>v;
+	 for(int i=0;i<k;i++)
+	 {
+	     v.push_back(arr[i]);
+	 }
+	 return v;
+	}
+
+};
