7660404531a2d951bbc3e7b4673acf04826b77c7
PURVA ATHNERE <93878803+purvathnere@users.noreply.github.com>
2024-01-23 19:19:29 +0530

    Create Parenthesis Checker.cpp

---
+++ Parenthesis Checker.cpp
@@ -0,0 +1,67 @@
+class Solution
+{
+    public:
+    //Function to check if brackets are balanced or not.
+  
+
+    
+bool ispar(string s)
+{
+    int n = s.size(); // Length of a string 
+    stack<char> st;    // Stack creation
+    bool ans = true;   // Suppose the string is already balanced
+
+    // Traverse the string 
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+        {
+            st.push(s[i]);
+        }
+        else if (s[i] == ')')
+        {
+            if (!st.empty() && st.top() == '(')
+            {
+                st.pop();
+            }
+            else
+            {
+                ans = false;
+                break;
+            }
+        }
+        else if (s[i] == '}')
+        {
+            if (!st.empty() && st.top() == '{')
+            {
+                st.pop();
+            }
+            else
+            {
+                ans = false;
+                break;
+            }
+        }
+        else if (s[i] == ']')
+        {
+            if (!st.empty() && st.top() == '[')
+            {
+                st.pop();
+            }
+            else
+            {
+                ans = false;
+                break;
+            }
+        }
+    }
+
+    return ans && st.empty();
+}
+
+
+
+
+    
+
+};
