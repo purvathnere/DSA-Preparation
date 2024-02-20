9df5213c86f042badc9f7197df3f6ff9385f503c
PURVA ATHNERE <93878803+purvathnere@users.noreply.github.com>
2024-02-21 00:53:02 +0530

    Create Findofocurrence.java

---
+++ Findofocurrence.java
@@ -0,0 +1,34 @@
+package com.myproject.recursion;
+
+
+public class Recursion {
+    public static int first = -1;
+    public static int last = -1;
+    public static void findOfOcurrence(String str,int idx,char element)
+    {
+        if(idx==str.length())
+        {
+            System.out.println(first);
+            System.out.println(last);
+            return;
+        }
+        char currChar = str.charAt(idx);
+        if(currChar == element)
+        {
+           if(first == -1 ) 
+           {
+               first = idx;
+           }
+           else
+           {
+               last = idx;
+           }
+        }
+        findOfOcurrence(str,idx+1,element);
+    }
+
+    public static void main(String[] args) {
+        String str = "abaacdaefaah";
+        findOfOcurrence(str,0,'a');
+    }
+}
