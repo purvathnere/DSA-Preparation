dd0eaa15dc0908f7ca820531580330e42b71d8f8
PURVA ATHNERE <93878803+purvathnere@users.noreply.github.com>
2024-03-26 01:03:54 +0530

    Create GraphImplementation.cpp

---
+++ GraphImplementation.cpp
@@ -0,0 +1,48 @@
+#include <algorithm>
+#include <iostream>
+#include <unordered_map>
+#include <list>
+
+using namespace std;
+
+class graph {
+public:
+    unordered_map<int, list<int>> adj; // Corrected declaration
+
+    void addEdge(int u, int v, bool direction) {
+        // direction = 0 for directed graph, 1 for undirected graph
+        adj[u].push_back(v);
+        if (direction==0) { // If it's a undirected graph
+            adj[v].push_back(u);
+        }
+    }
+
+    void printAdjList() {
+        for (auto i : adj) {
+            cout << i.first << "-> ";
+            for (auto j : i.second) {
+                cout << j << " , ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+int main() {
+    int n;
+    cout << "Enter the Number of Nodes" << endl;
+    cin >> n;
+    int m;
+    cout << "Enter the Number of edges" << endl;
+    cin >> m;
+    graph g;
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        // Creating an undirected graph
+        g.addEdge(u, v, 0); // Passing 0 for undirected graph
+    }
+    g.printAdjList();
+
+    return 0;
+}
